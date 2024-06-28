4174ecdff12d8d61fa50e00dd2202d7a8027261a
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-06-28 19:10:11 +0600

    Initial Commit

---
+++ WhatsApp-Clone/WhatsApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CloneApp.swift
+//  WhatsApp-Clone
+//
+//  Created by shopnil hasan on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
