6e99f7a033872128934f1e9e7b7a4f938fab9a85
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-07-02 17:59:42 +0600

    chat room Toolbar item complete

---
+++ WhatsApp-Clone/ChatRoom/View/ChatRoomView.swift
@@ -0,0 +1,52 @@
+//
+//  ChatRoomView.swift
+//  WhatsApp-Clone
+//
+//  Created by shopnil hasan on 2/7/24.
+//
+
+import SwiftUI
+
+struct ChatRoomView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<12) {_ in
+                Text("placeholder")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack {
+                        Circle()
+                            .frame(width: 35, height: 35)
+                        Text("User Name")
+                            .bold()
+                    }
+                }
+                
+                ToolbarItemGroup {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "video")
+                    }
+
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "phone")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+#Preview {
+    NavigationStack {
+        ChatRoomView()
+    }
+}
