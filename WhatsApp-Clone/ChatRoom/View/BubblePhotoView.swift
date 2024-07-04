97e877ce8b62041fa86e5a69fd412c470ba4c04e
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-07-04 19:18:27 +0600

    Bubble Photo View

--- WhatsApp-Clone/ChatRoom/View/BubblePhotoView.swift
+++ WhatsApp-Clone/ChatRoom/View/BubblePhotoView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 struct BubblePhotoView: View {
     let item: MessageBubbleItem
     var body: some View {
-        Text(item.text)
+        HStack {
+            if item.direction == .sent {Spacer()}
+            
+            HStack {
+                if item.direction == .sent {shareButton()}
+                messagePhotoView()
+                if item.direction == .received {shareButton()}
+            }
+            
+            if item.direction == .received {Spacer()}
+        }
+    }
+    
+    
+    private func messagePhotoView() -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Image(systemName: "photo.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 220, height: 180)
+                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+        }
+    }
+    private func shareButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "arrowshape.turn.up.right.fill")
+                .foregroundStyle(.whatsAppWhite)
+                .padding(8)
+                .background(.gray)
+                .background(.thinMaterial)
+                .clipShape(Circle())
+
+        }
     }
 }
 
