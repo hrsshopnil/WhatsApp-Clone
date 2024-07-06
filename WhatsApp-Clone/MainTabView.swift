9db9f8ce6e70c54586b2b68cd1b682a2ef82db03
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-07-06 18:42:51 +0600

    slider Thumb image fix

--- WhatsApp-Clone/MainTabView.swift
+++ WhatsApp-Clone/MainTabView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct MainTabView: View {
     init() {
         makeTabBarOpaque()
+        let thumbImage = UIImage(systemName: "circle.fill")
+        UISlider.appearance().setThumbImage(thumbImage, for: .normal)
     }
     var body: some View {
         TabView {
