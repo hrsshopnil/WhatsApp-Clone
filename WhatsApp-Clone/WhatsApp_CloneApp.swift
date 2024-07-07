b0d4dbb26079615e57520a1f255a5b34e5e91c60
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-07-07 16:30:44 +0600

    Firebase SDK add

--- WhatsApp-Clone/WhatsApp_CloneApp.swift
+++ WhatsApp-Clone/WhatsApp_CloneApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct WhatsApp_CloneApp: App {
-    var body: some Scene {
-        WindowGroup {
-            MainTabView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+        MainTabView()
     }
+  }
 }
