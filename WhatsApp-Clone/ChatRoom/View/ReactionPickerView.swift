427e3c5e4645f06181a68b960a27fe23a6e263df
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-08-27 23:46:26 +0600

    Reaction Picker View

--- WhatsApp-Clone/ChatRoom/View/ReactionPickerView.swift
+++ WhatsApp-Clone/ChatRoom/View/ReactionPickerView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+struct EmojiReaction {
+    let reaction: Reaction
+    var isAnimating = false
+    var opacity: CGFloat = 1
+}
 struct ReactionPickerView: View {
+    
+    @State private var emojiState: [EmojiReaction] = [
+        EmojiReaction(reaction: .like),
+        EmojiReaction(reaction: .love),
+        EmojiReaction(reaction: .laugh),
+        EmojiReaction(reaction: .shocked),
+        EmojiReaction(reaction: .sad),
+        EmojiReaction(reaction: .pray),
+        EmojiReaction(reaction: .more)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            ForEach(Array(emojiState.enumerated()), id: \.offset) {index, item in
+                Text(item.reaction.emoji)
+                    .font(.system(size: 30))
+            }
+        }
     }
 }
 
